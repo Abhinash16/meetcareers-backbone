b794bc97bfda4fe069c048693c561ea449c41b5b
Abhinash Sharma <52877872+Abhinash16@users.noreply.github.com>
2023-07-26 09:18:16 +0530

    push updated

--- src/app.js
+++ src/app.js
@@ -1,41 +1,45 @@
 const express = require("express");
 const { json, urlencoded } = require("body-parser");
 var cors = require("cors");
-var mongoUtil = require("../config/mongoUtil");
-require("../config/firebaseUtil");
+// var mongoUtil = require("../config/mongoUtil");
+// require("../config/firebaseUtil");
 
-const registerUsersRoutes = require("./routes/users");
-const registerInboxRoutes = require("./routes/inbox");
+// const registerUsersRoutes = require("./routes/users");
+// const registerInboxRoutes = require("./routes/inbox");
 
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+// const io = require("socket.io")(http);
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(cors());
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  // Handle incoming messages
-  socket.on("chat message", (message) => {
-    console.log("Message received: ", message);
-    io.emit("chat message", message);
-  });
+// io.on("connection", (socket) => {
+//   console.log("A user connected");
+//   // Handle incoming messages
+//   socket.on("chat message", (message) => {
+//     console.log("Message received: ", message);
+//     io.emit("chat message", message);
+//   });
 
-  // Handle disconnections
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
+//   // Handle disconnections
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected");
+//   });
+// });
+
+// mongoUtil.connectToServer(function (err, client) {
+//   if (err) console.log(err);
+// });
+
+// app.use(require("./routes/blogs"));
+
+// registerUsersRoutes(app);
+// registerInboxRoutes(app);
+
+app.get("/", (req, res) => {
+  res.send("I am being called");
 });
-
-mongoUtil.connectToServer(function (err, client) {
-  if (err) console.log(err);
-});
-
-app.use(require("./routes/blogs"));
-
-registerUsersRoutes(app);
-registerInboxRoutes(app);
 
 const port = process.env.PORT || 3100;
 
